8d0532b55fefc9b5607258ffb61639d1c740127f
ephraimjensen <jen22082@byui.edu>
2025-06-06 15:43:09 -0600

    added commented code to make it easier to demo

--- week6/chat_app/src/chat_app_app.erl
+++ week6/chat_app/src/chat_app_app.erl
@@ -12,5 +12,11 @@ stop(_State) ->
     ok.
 % chat_client:start("bob").
 % chat_client:start("alice").
+
+% chat_client:send("bob", {"alice", "Hello Alice!!"}).
+% chat_client:send("alice", {"bob", "Howdy Bob."}).
+% chat_client:send("bob", {"steven", "Are you there Steven?"}).
+% 
 % chat_client:start("steven").
-% chat_client:start("brenda").
+% chat_client:send("bob", {"steven", "Are you there Steven?"}).
+% chat_client:send("steven", {"bob", "I'm here now"}).
